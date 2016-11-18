04a64db92d32ae3eca26a8aa286f58635233e067
almeidaah <almeidascvn@gmail.com>
2016-11-18 08:49:55 -0200

    PAdronizando dados e Print de lista

--- root-sourcefolder/readAndSplit.py
+++ root-sourcefolder/readAndSplit.py
@@ -18,13 +18,26 @@ with open('../files/sarah.txt') as saf:
 sarah = data.strip().split(',')
 
 #print lista original
+print("Original James List")
 print(james)
 #printando a lista copiada dos dados originais, mantem a lista original
+print("Sorted James List")
 print(sorted(james))
-print("\n")
-print(julie)
-print(mikey)
-print(sorted(sarah))
+#print("\n")
+#print(julie)
+#print(mikey)
+#print(sorted(sarah))
 
+def padronize(item):
+    if '-' in item:
+        splitter = '-'
+    elif ':' in item:
+        splitter = ':'
+    else:
+        return(item)
+    (min,secs) = item.split(splitter)
+    return (min + '.' + secs)
 
+print("Lista Padronizada e sem duplicatas (set()) ")
+print(sorted(set([padronize(item) for item in james])))
 
